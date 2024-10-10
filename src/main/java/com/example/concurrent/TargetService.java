da4a58febcc86d0256cc398048fc830eaf64152b
Ehigh <rkd2rb@naver.com>
2024-10-10 17:56:37 +0900

    Fix: TargetService 로직에서의 동시성 문제 해결
    ReadWriteLock을 적용해서 read작업과 write작업의 동시 실행 방지
    적용 전 : 성공 약 1890 / 실패 약 110
    적용 후 : 성공 2000 / 실패 0

--- src/main/java/com/example/concurrent/TargetService.java
+++ src/main/java/com/example/concurrent/TargetService.java
@@ -3,14 +3,24 @@ package com.example.concurrent;
 import com.example.concurrent.model.A;
 import com.example.concurrent.model.ReadResult;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class TargetService {
+
     private A A;
+    // 공정성 유지; 특정 작업이 오래 밀리면 안됨
+    private ReadWriteLock lock = new ReentrantReadWriteLock(true);
+    private Lock readLock = lock.readLock();
+    private Lock writeLock = lock.writeLock();
 
     public TargetService(A a) {
         A = a;
     }
 
     public ReadResult read() {
+        readLock.lock();
         try {
             int a = A.getA();
             Thread.sleep(0, 1);
@@ -18,8 +28,10 @@ public class TargetService {
             Thread.sleep(0, 1);
             String c = A.getC();
             return new ReadResult(a, b, c);
-        } catch (Exception e) {
+        } catch (InterruptedException e) {
 
+        } finally {
+            readLock.unlock();
         }
         return null;
     }
@@ -28,6 +40,8 @@ public class TargetService {
      * A 안의 값을 1씩 올린다.
      */
     public void update() {
+        writeLock.lock();
         A.increase();
+        writeLock.unlock();
     }
 }
